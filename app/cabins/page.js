a5de3afc75c760af521b1504ba63a0143a5e341f
Mo2men7 <momen.helmy.m@gmail.com>
2024-08-07 03:59:25 +0300

    Sharing State Between Client and Server: The URL

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,19 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 // export const revalidate = 0;
-export const revalidate = 3600;
+// export const revalidate = 3600;
+// we disabled this as the page is no longer static after filtering
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +28,12 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
